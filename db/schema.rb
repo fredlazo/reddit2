7abda0ef56ea7301e31e6dd97bfad9e81a044bfd
Frederick Lazo <fredlazo@yahoo.com>
2016-09-02 23:16:46 -0700

    Checkpoint 43 completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160828045012) do
+ActiveRecord::Schema.define(version: 20160903000247) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20160828045012) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
@@ -95,5 +96,16 @@ ActiveRecord::Schema.define(version: 20160828045012) do
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,13 +27,18 @@ topics = Topic.all
 
 
 50.times do
-  Post.create!(
+  post = Post.create!(
     user: users.sample,
     topic: topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
+
+post.update_attribute(:created_at, rand(10.minutes..1.year).ago)
+
+rand(1..5).times {post.votes.create(value: [-1,1].sample, user: users.sample)}
 end
+
 posts = Post.all
 
 # Create Comments
@@ -65,5 +70,6 @@ puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{Vote.count} votes created"
 puts "#{Comment.count} comments created"
 puts "#{Advertisement.count} ads created"
